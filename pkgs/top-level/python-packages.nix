34da5ac7c299cd4b46854601a37ec8fa7ace03c2
George Huebner <george@feyor.sh>
2026-02-04 13:21:47 -0600

    unicorn-angr: remove

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -20596,10 +20596,6 @@ self: super: with self; {
 
   unicorn = callPackage ../development/python-modules/unicorn { inherit (pkgs) unicorn; };
 
-  unicorn-angr = callPackage ../development/python-modules/unicorn-angr {
-    inherit (pkgs) unicorn-angr;
-  };
-
   unicrypto = callPackage ../development/python-modules/unicrypto { };
 
   unicurses = callPackage ../development/python-modules/unicurses { };
