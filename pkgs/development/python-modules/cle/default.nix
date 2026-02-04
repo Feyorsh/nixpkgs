19bc91e6d22c3f094fe2180b65df25b0a6519092
George Huebner <george@feyor.sh>
2026-02-04 17:42:00 -0600

    python3Packages.cle: 9.2.154 -> 9.2.197

--- pkgs/development/python-modules/cle/default.nix
+++ pkgs/development/python-modules/cle/default.nix
@@ -1,4 +1,6 @@
 {
+  stdenv,
+  fetchpatch,
   lib,
   archinfo,
   buildPythonPackage,
@@ -8,23 +10,27 @@
   pefile,
   pyelftools,
   pytestCheckHook,
-  pythonOlder,
   pyvex,
   setuptools,
   sortedcontainers,
   nix-update-script,
+  arpy,
+  minidump,
+  pyxbe,
+  pyxdia,
+  uefi-firmware-parser,
 }:
 
 let
   # The binaries are following the argr projects release cycle
-  version = "9.2.154";
+  version = "9.2.197";
 
   # Binary files from https://github.com/angr/binaries (only used for testing and only here)
   binaries = fetchFromGitHub {
     owner = "angr";
     repo = "binaries";
     tag = "v${version}";
-    hash = "sha256-XXJBySIT3ylK1nd3suP2bq4bVSVah/1XhOmkEONbCoY=";
+    hash = "sha256-x5Ot4UlJelvYANQc8h0O6FlMEEKtdWDrrQ1ku1cwey4=";
   };
 in
 buildPythonPackage rec {
@@ -32,14 +38,23 @@ buildPythonPackage rec {
   inherit version;
   pyproject = true;
 
-  disabled = pythonOlder "3.11";
-
   src = fetchFromGitHub {
     owner = "angr";
     repo = "cle";
     tag = "v${version}";
-    hash = "sha256-rWbZzm5hWi/C+te8zeQChxqYHO0S795tJ6Znocq9TTs=";
+    hash = "sha256-8hA4r1y5tItyWPGJCMQnmLx1fRfEGjmGH86x+9WqSRQ=";
   };
+
+  patches = lib.optionals stdenv.hostPlatform.isDarwin [
+    (fetchpatch {
+      url = "https://github.com/angr/cle/commit/6d41df8b0d2523b5a6c1e7c80a0be9d439c72642.patch";
+      hash = "sha256-c+I0jxlKsoPFX7DK/icZ+3QrAAQRjuXOlWdKrH5jMNw=";
+      revert = true;
+    })
+  ];
+
+  pythonRelaxDeps = [ "arpy" ];
+  pythonRemoveDeps = lib.optionals stdenv.hostPlatform.isDarwin [ "pyxdia" ];
 
   build-system = [ setuptools ];
 
@@ -51,6 +66,13 @@ buildPythonPackage rec {
     pyelftools
     pyvex
     sortedcontainers
+    arpy
+    minidump
+    pyxbe
+    uefi-firmware-parser
+  ]
+  ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [
+    pyxdia
   ];
 
   nativeCheckInputs = [ pytestCheckHook ];
@@ -73,6 +95,10 @@ buildPythonPackage rec {
     "test_x86_64"
     # The required parts is not present on Nix
     "test_remote_file_map"
+  ];
+
+  disabledTestPaths = lib.optionals stdenv.hostPlatform.isDarwin [
+    "tests/test_pe.py"
   ];
 
   pythonImportsCheck = [ "cle" ];
