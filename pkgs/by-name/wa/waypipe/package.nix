6437582bf7d5f7489be1cebc39050345fa4f43d5
George Huebner <george@feyor.sh>
2026-04-12 12:59:26 -0500

    waypipe: add aarch64-darwin support from cocoa-way

--- pkgs/by-name/wa/waypipe/package.nix
+++ pkgs/by-name/wa/waypipe/package.nix
@@ -2,6 +2,7 @@
   lib,
   rustPlatform,
   fetchFromGitLab,
+  fetchpatch,
   meson,
   ninja,
   pkg-config,
@@ -32,9 +33,29 @@ llvmPackages.stdenv.mkDerivation (finalAttrs: {
     tag = "v${finalAttrs.version}";
     hash = "sha256-Tbd/yY90yb2+/ODYVL3SudHaJCGJKatZ9FuGM2uAX+8=";
   };
+
+  cargoPatches = lib.optionals (llvmPackages.stdenv.hostPlatform.isDarwin) [
+    (fetchpatch {
+      url = "https://github.com/J-x-Z/waypipe-darwin/commit/38d4b535e0e0168cfba0cb1ad6ec846253629f09.patch";
+      hash = "sha256-UJXToroFteOkslfRy8i4yzb2Q4bWWKVs22wKQ6dGBr8=";
+    })
+  ];
+
+  patches = lib.optionals (llvmPackages.stdenv.hostPlatform.isDarwin) [
+    (fetchpatch {
+      url = "https://github.com/J-x-Z/waypipe-darwin/commit/38d4b535e0e0168cfba0cb1ad6ec846253629f09.patch";
+      hash = "sha256-aioIUTwl9/Z2CKagSYHsaz6yOZKxt0O5fFRtsjIeKSE=";
+      excludes = [ "Cargo.lock" ];
+    })
+  ];
+
+  postPatch = ''
+    sed -e '/gbmfallback/d' -i meson.build
+  '';
+
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) pname version src;
-    hash = "sha256-IUvXHLxrhc2Au57wsE53Q+NL1cZzFcaRG3HDV8s3xWw=";
+    hash = if (llvmPackages.stdenv.hostPlatform.isDarwin) then "sha256-IUvXHLxrhc2Au57wsE53Q+NL1cZzFcaRG3HDV8s3xWw=" else "sha256-IUvXHLxrhc2Au57wsE53Q+NL1cZzFcaRG3HDV8s3xWw=";
   };
 
   strictDeps = true;
@@ -51,23 +72,32 @@ llvmPackages.stdenv.mkDerivation (finalAttrs: {
     rustc
     wayland-scanner
     rustPlatform.cargoSetupHook
-    autoPatchelfHook
     rust-bindgen
+  ] ++ lib.optionals (llvmPackages.stdenv.hostPlatform.isLinux) [
+    autoPatchelfHook
   ];
 
   buildInputs = [
-    libgbm
     lz4
     zstd
     ffmpeg
     vulkan-headers
     vulkan-loader
+  ] ++ lib.optionals (llvmPackages.stdenv.hostPlatform.isLinux) [
+    libgbm
   ];
 
   runtimeDependencies = [
-    libgbm
     ffmpeg.lib
     vulkan-loader
+  ] ++ lib.optionals (llvmPackages.stdenv.hostPlatform.isLinux) [
+    libgbm
+  ];
+
+  mesonFlags = lib.optionals (llvmPackages.stdenv.hostPlatform.isDarwin) [
+    (lib.mesonEnable "with_gbm" false)
+    (lib.mesonEnable "with_video" false)
+    (lib.mesonEnable "with_dmabuf" false)
   ];
 
   passthru.updateScript = nix-update-script { };
@@ -82,7 +112,7 @@ llvmPackages.stdenv.mkDerivation (finalAttrs: {
     homepage = "https://mstoeckl.com/notes/gsoc/blog.html";
     changelog = "https://gitlab.freedesktop.org/mstoeckl/waypipe/-/releases#v${finalAttrs.version}";
     license = lib.licenses.mit;
-    platforms = lib.platforms.linux;
+    platforms = lib.platforms.unix;
     maintainers = with lib.maintainers; [ mic92 ];
     mainProgram = "waypipe";
   };
